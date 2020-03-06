fb03739a466b9af6a069290de0d098e8b6e7319d
adityabisoi <adityabisoi1999@gmail.com>
2020-03-06 21:55:56 +0530

    links to articles added

--- frontend/gui/src/components/Article.js
+++ frontend/gui/src/components/Article.js
@@ -44,7 +44,7 @@ function Article(props){
             >
                 <List.Item.Meta
                 avatar={<Avatar src={item.avatar} />}
-                title={<a href={item.href}>{item.title}</a>}
+                title={<a href={`/${item.id}`}>{item.title}</a>}
                 description={item.description}
                 />
                 {item.content}
