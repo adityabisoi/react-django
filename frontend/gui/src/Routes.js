202a567324eba78c1e0fc40ce8db2e60817aebce
adityabisoi <adityabisoi1999@gmail.com>
2020-03-06 21:43:50 +0530

    ArticleDetailView added

--- frontend/gui/src/Routes.js
+++ frontend/gui/src/Routes.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
-import ArticleListView from './containers/ArticleListView'; 
+import ArticleListView from './containers/ArticleListView'
+import ArticleDetailView from './containers/ArticleDetailView'
 
 function BaseRouter(){
     return(
         <div>
             <Route exact path="/" component={ArticleListView} />
-            <Route exact path="/:articleID" component={ArticleListView} />
+            <Route exact path="/:articleID" component={ArticleDetailView} />
         </div>
     );
 }
